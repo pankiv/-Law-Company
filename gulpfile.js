2691383f569bed3ac289c7a8b19f129c3f379378
grawdanin <terjustadm@gmail.com>
2016-07-29 15:29:37 +0400

    with massonary

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,9 @@ gulp.task('scripts', function() {
 		'./app/libs/jquery/dist/jquery.min.js',
 		'./app/libs/owl.carousel/dist/owl.carousel.min.js',
 		'./app/libs/superfish/dist/js/superfish.min.js',
-		'./app/libs/jQuery.mmenu/dist/js/jquery.mmenu.all.min.js'
+		'./app/libs/jQuery.mmenu/dist/js/jquery.mmenu.all.min.js',
+		'./app/libs/particles.js/particles.min.js',
+		'./app/libs/salvattore/dist/salvattore.min.js'
 
 		])
 		.pipe(concat('libs.js'))
